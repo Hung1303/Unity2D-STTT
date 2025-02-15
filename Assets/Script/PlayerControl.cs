aa5639d45aa5dec4c39f65c294a2b2ddd0787b72
Hung1303 <hungdtse172297@fpt.edu.vn>
2025-02-15 09:49:25 +0700

    Unity setup

---
+++ Assets/Script/PlayerControl.cs
@@ -0,0 +1,58 @@
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+	public Rigidbody2D rb;
+
+	public float jumpHeight = 5f;
+	private bool isGround = true;
+
+	public float movement;
+	public float speed = 5f;
+	private bool facingRight = true;
+
+	private void Start()
+	{
+		rb = GetComponent<Rigidbody2D>();
+	}
+
+	void Update()
+    {
+		movement = Input.GetAxis("Horizontal");
+
+		if (movement < 0f && facingRight)
+		{
+			transform.eulerAngles = new Vector3(0f, -180f, 0f);
+			facingRight = false;
+		}
+		else if (movement > 0f && facingRight == false)
+		{
+			transform.eulerAngles = new Vector3(0f, 0f, 0f);
+			facingRight = true;
+		}
+
+		if (Input.GetKeyDown(KeyCode.Space) && isGround)
+		{
+			Jump();
+			isGround = false;
+		}
+	}
+
+	private void FixedUpdate()
+	{
+		transform.position += new Vector3(movement, 0f, 0f) * Time.deltaTime * speed;
+	}
+
+	void Jump()
+	{
+		rb.AddForce(new Vector2(0f, jumpHeight), ForceMode2D.Impulse);
+	}
+
+	private void OnCollisionEnter2D(Collision2D collision)
+	{
+		if (collision.gameObject.tag == "Ground")
+		{
+			isGround = true;
+		}
+	}
+}
